ca6c896bb939aa1956c77100c8b542d9aa10bd02
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2018-08-28 17:20:09 -0300

    Sumarized js modal opening inside modalTrigger function

--- js/modal_scheduling.js
+++ js/modal_scheduling.js
@@ -5,41 +5,25 @@ editButton = document.querySelectorAll('#scheduleCardButtonsEdit');
 
 
 addSchedulingButton.addEventListener("click", function(){
-  if ( addSchedulingButton.style.transform == "rotate(45deg)" ) {
-    addSchedulingButton.style.transform = "rotate(0)";
-  }
-  else { addSchedulingButton.style.transform = "rotate(45deg)"; }
+  modalTrigger();
 });
 
-for (i = 0; i < editButton.length; i++) {
-  editButton[i].addEventListener("click", function() {
-    if (addSchedulingButton.style.transform == "rotate(45deg)") {
-      addSchedulingButton.style.transform = "rotate(0)";
-    } else {
-      addSchedulingButton.style.transform = "rotate(45deg)";
-    }
-  });
-}
-
-addSchedulingButton.addEventListener("click", function(){
-  if (modalContainer.style.display == "flex") {
-    modalContainer.style.display = "none";
-  } else {
-    modalContainer.style.display = "flex";
-  }
+modalDarken.addEventListener("click", function(){
+  modalTrigger();
 });
 
 for (i = 0; i < editButton.length; i++) {
   editButton[i].addEventListener("click", function(){
-    if (modalContainer.style.display == "flex") {
-      modalContainer.style.display = "none";
-    } else {
-      modalContainer.style.display = "flex";
-    }
+    modalTrigger();
   });
 }
 
-modalDarken.addEventListener("click", function(){
-  modalContainer.style.display = "none";
-  addSchedulingButton.style.transform = "rotate(0)";
-});
+function modalTrigger() {
+  if ( addSchedulingButton.style.transform == "rotate(45deg)" ) {
+    addSchedulingButton.style.transform = "rotate(0)";
+  } else { addSchedulingButton.style.transform = "rotate(45deg)"; }
+
+  if (modalContainer.style.display == "flex") {
+    modalContainer.style.display = "none";
+  } else { modalContainer.style.display = "flex"; }
+}
